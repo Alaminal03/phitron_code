ebc53d2e2f48ceba093478d71caad0ed66eef13b
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-05-12 02:26:12 +0600

    Add files via upload

---
+++ recurtion_2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void hello(int i)
+{
+    if(i==5)
+    {
+        return;
+    }
+    printf("%d\n", i);
+    hello(i+1);
+}
+
+int main()
+{
+    int i = 0;
+    hello(i);
+
+    return 0;
+}
