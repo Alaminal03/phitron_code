87f41bc5f0fb0c02c6bb1fe9e207fbec045c823f
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-05-12 15:37:38 +0600

    Add files via upload

---
+++ 2D_array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int x, y;
+    scanf("%d%d", &x, &y);
+
+    int a[x][y];
+
+    for (int i = 0; i < x;i++)
+    {
+        for (int j = 0; j < y;j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < x;i++)
+    {
+        for (int j = 0; j < y;j++)
+        {
+            printf(" %d ", a[i][j]);
+        }
+    }
+
+        return 0;
+}
